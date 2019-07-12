02df2a10955aa3a4ae23e1e07256eeeb017bacbb
bank8426 <bank_55555@hotmail.com>
2019-07-12 21:27:16 +0700

    sending tweets

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule PluralsightTweet.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :quantum],
+      extra_applications: [:logger, :quantum,
+      :extwitter],
       mod: {PluralsightTweet.Application, []}
     ]
   end
@@ -23,7 +24,8 @@ defmodule PluralsightTweet.MixProject do
   defp deps do
     [
       {:credo, "~>0.4.11"},
-      {:quantum, "~> 1.8"}
+      {:quantum, "~> 1.8.0"},
+      {:extwitter, "~> 0.9.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
